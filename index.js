6cc71beb8114cae6e5711541a01f9f12e7b897e6
Angelo-Pikaso23 <espinozagutierrezemiliano@gmail.com>
2025-08-14 17:05:04 -0600

    Microservicio del analizador de encabezados de solicitud

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
 // index.js
-// where your node app starts
+// donde inicia tu aplicación node
 
-// init project
+// inicializar proyecto
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// habilitar CORS (https://es.wikipedia.org/wiki/Intercambio_de_recursos_de_origen_cruzado)
+// para que tu API pueda ser probada remotamente por FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200}));  // algunos navegadores antiguos fallan con 204
 
-// http://expressjs.com/en/starter/static-files.html
+// http://expressjs.com/es/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// http://expressjs.com/es/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// tu primer endpoint de API...
+app.get("/api/:date?", function (req, res) {
+  let dateStg= req.params.date;
+  let date;
+  if (!dateStg) {
+    date = new Date();
+  } else if (!isNaN(dateStg)) {
+    date = new Date(parseInt(dateStg));
+  } else {
+    date = new Date(dateStg);
+  }
+  if (date.toString() === 'Invalid Date') {
+    return res.json({ error: "Invalid Date" });
+  }
+  return res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
-
-
-
-// Listen on port set in environment variable or default to 3000
+// Escuchar en el puerto establecido en la variable de entorno o por defecto en 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log('Tu aplicación está escuchando en el puerto ' + listener.address().port);
 });
